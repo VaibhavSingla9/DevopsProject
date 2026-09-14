36f167e03a94f21ee194f9de3cebd7f985e85ff2
Vaibhav Singla <vaibhavsingla456@gmail.com>
2026-09-14 11:59:21 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("DevOps application is running");
+});
+
+app.get("/health", (req, res) => {
+    res.json({ status: "healthy" });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
